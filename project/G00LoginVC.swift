cd99a07052ccfd74f0e4f148fb509110b542bd56
SPJ <spj@spjs-mac-mini.local>
2016-12-21 14:30:52 +0700

    - Add harpyframework - Fix bug BUG0025

--- project/G00LoginVC.swift
+++ project/G00LoginVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import harpyframework
 
 class G00LoginVC: CommonViewController, UIPopoverPresentationControllerDelegate, UITextFieldDelegate {
     // MARK: Properties
@@ -22,7 +23,8 @@ class G00LoginVC: CommonViewController, UIPopoverPresentationControllerDelegate,
     /** Sign in button */
     @IBOutlet weak var btnSignin: UIButton!
     /** Show password checkbox */
-    @IBOutlet weak var chbShowPassword: CheckBox!
+    //@IBOutlet weak var chbShowPassword: CheckBox!
+    @IBOutlet weak var chbShowPassword: CustomCheckBox!
     /** Show password label */
     @IBOutlet weak var lblShowPassword: UILabel!
     /** Tap counter on logo */
