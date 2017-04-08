486956c61e1e8b6a749512cb963b2d851138c09c
Nixforest <Nixforest21991920@gmail.com>
2017-04-08 16:45:48 +0700

    BUG0054
    BUG0054

---
+++ project/G07/F00/G07F00S01VC.swift
@@ -0,0 +1,174 @@
+//
+//  G07F00S01VC.swift
+//  project
+//
+//  Created by SPJ on 4/7/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+import harpyframework
+
+class G07F00S01VC: ParentViewController, UITableViewDelegate, UITableViewDataSource {
+    // MARK: Properties
+    /** Segment button */
+    private var _segment:           UISegmentedControl  = UISegmentedControl(
+        items: [
+            DomainConst.CONTENT00074,
+            DomainConst.CONTENT00311
+        ])
+    /** Table view */
+    private var _tblView:           UITableView         = UITableView()
+    /** Data */
+    private var _data:              OrderListRespModel  = OrderListRespModel()
+    /** Page number */
+    private var _page:              Int                 = 0
+    /**
+     * Type: New (0) or Finish (1)
+     */
+    private var _type:              Int                 = 0
+    /** Refrest control */
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh(_:)), for: .valueChanged)
+        return refreshControl
+    }()
+    
+    // MARK: Methods
+    /**
+     * Request data from server
+     */
+    private func requestData(action: Selector = #selector(setData(_:))) {
+        OrderFamilyListRequest.request(action: action,
+                                       view: self,
+                                       page: String(_page))
+    }
+    private func resetData() {
+        _data.clearData()
+        // Reset current search value
+        self._page      = 0
+        // Reload table
+        _tblView.reloadData()
+    }
+    
+    
+    /**
+     * Handle refresh
+     */
+    internal func handleRefresh(_ sender: AnyObject) {
+        self.resetData()
+        requestData(action: #selector(finishHandleRefresh(_:)))
+    }
+    
+    internal func finishHandleRefresh(_ notification: Notification) {
+        setData(notification)
+        refreshControl.endRefreshing()
+    }
+    
+    /**
+     * Handle change value of segment
+     */
+    internal func segmentChange(_ sender: AnyObject) {
+        _type = _segment.selectedSegmentIndex
+        switch _type {
+        case 0:
+            break
+        case 1:
+            break
+        default:
+            break
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // Navigation
+        createNavigationBar(title: DomainConst.CONTENT00310)
+        
+        // Create content
+        var offset: CGFloat = getTopHeight()
+        
+        // Segment
+        let font = UIFont.systemFont(ofSize: GlobalConst.BASE_FONT_SIZE)
+        _segment.frame = CGRect(x: 0, y: offset,
+                                width: GlobalConst.SCREEN_WIDTH,
+                                height: GlobalConst.BUTTON_H)
+        _segment.setTitleTextAttributes([NSFontAttributeName: font],
+                                        for: UIControlState())
+        _segment.selectedSegmentIndex = 0
+        _segment.layer.borderWidth = GlobalConst.BUTTON_BORDER_WIDTH
+        _segment.layer.borderColor = GlobalConst.BUTTON_COLOR_RED.cgColor
+        _segment.tintColor = GlobalConst.BUTTON_COLOR_RED
+        _segment.addTarget(self, action: #selector(segmentChange(_:)), for: .valueChanged)
+        offset = offset + _segment.frame.height
+        self.view.addSubview(_segment)
+        
+        // Table View
+        _tblView.register(UINib(nibName: OrderEmployeeTableViewCell.theClassName,
+                                bundle: Bundle(identifier: DomainConst.HARPY_FRAMEWORK_BUNDLE_NAME)),
+                          forCellReuseIdentifier: OrderEmployeeTableViewCell.theClassName)
+        _tblView.delegate = self
+        _tblView.dataSource = self
+        _tblView.frame = CGRect(x: 0, y: offset,
+                                width: GlobalConst.SCREEN_WIDTH,
+                                height: GlobalConst.SCREEN_HEIGHT - offset)
+        _tblView.addSubview(refreshControl)
+        self.view.addSubview(_tblView)
+        
+        // Request data from server
+        requestData()
+    }
+    
+    override func setData(_ notification: Notification) {
+        let data = (notification.object as! String)
+        let model = OrderListRespModel(jsonString: data)
+        if model.isSuccess() {
+            _data.total_page = model.total_page
+            _data.total_record = model.total_record
+            _data.append(contentOf: model.getRecord())
+            _tblView.reloadData()
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    // MARK: - UITableViewDataSource
+    /**
+     * Tells the data source to return the number of rows in a given section of a table view.
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return _data.getRecord().count
+    }
+    
+    /**
+     * Asks the data source for a cell to insert in a particular location of the table view.
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
+        UITableViewCell {
+            let cell: OrderEmployeeTableViewCell = tableView.dequeueReusableCell(
+                withIdentifier: OrderEmployeeTableViewCell.theClassName)
+                as! OrderEmployeeTableViewCell
+            cell.setData(data: _data.getRecord()[indexPath.row])
+            return cell
+    }/**
+     * Asks the delegate for the height to use for a row in a specified location.
+     */
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return OrderEmployeeTableViewCell.CELL_HEIGHT
+    }
+}
---
+++ project/G07/G07Const.swift
@@ -0,0 +1,15 @@
+//
+//  G07Const.swift
+//  project
+//
+//  Created by SPJ on 4/7/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+import harpyframework
+
+class G07Const: NSObject {
+    /** Function identifier */
+    public static let FUNC_IDENTIFIER                           = DomainConst.APPNAME + "g07"
+}
